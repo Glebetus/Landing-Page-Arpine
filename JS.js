637b04ebe1664b356de9f2d254c0eb14775e9441
Glebetus <158508376+Glebetus@users.noreply.github.com>
2024-02-01 15:40:44 -0500

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,157 @@
+
+
+const track = document.getElementById("image-track",);
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 1;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image",)) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+
+/* -- Had to add extra lines for touch events -- */
+
+track.onmousedown = e => handleOnDown(e);
+
+track.ontouchstart = e => handleOnDown(e.touches[0]);
+
+track.onmouseup = e => handleOnUp(e);
+
+track.ontouchend = e => handleOnUp(e.touches[0]);
+
+track.onmousemove = e => handleOnMove(e);
+
+track.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const logo = document.getElementById("logo"),
+      images = logo.querySelectorAll("img");
+
+const getActive = () => document.body.dataset.active === "true",
+      setActiveTo = active => document.body.dataset.active = active;
+
+const shift = (image, index, rangeX, rangeY) => {
+  const active = getActive();
+        
+  const translationIntensity = active ? 4 : 4,
+        maxTranslation = translationIntensity * (index + 1),
+        currentTranslation = `${maxTranslation * rangeX}% ${maxTranslation * rangeY}%`;
+  
+  const scale = active ? 1 + (index * 0.06) : 1;
+  
+  image.animate({ 
+    translate: currentTranslation, 
+    scale 
+  }, { duration: 750, fill: "forwards", easing: "ease" });
+}
+
+const shiftAll = (images, rangeX, rangeY) => 
+  images.forEach((image, index) => shift(image, index, rangeX, rangeY));
+
+const shiftLogo = (e, images) => {  
+  const rect = logo.getBoundingClientRect(),
+        radius = 1000;
+  
+  const centerX = rect.left + (rect.width / 2),
+        centerY = rect.top + (rect.height / 2);
+  
+  const rangeX = (e.clientX - centerX) / radius,
+        rangeY = (e.clientY - centerY) / radius;
+  
+  shiftAll(images, rangeX, rangeY);
+}
+
+const resetLogo = () => {
+  setActiveTo(false);
+  shiftAll(images, 0.4, -0.7);
+}
+
+window.onmousemove = e => shiftLogo(e, images);
+
+
+document.body.onmouseleave = () => {
+  if(!getActive()) resetLogo();
+}
+
+window.onmousedown = e => {
+  setActiveTo(true);
+  shiftLogo(e, images);
+}
+
+window.onmouseup = e => resetLogo();
+
+resetLogo();
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const enhance = id => {
+  const element = document.getElementById(id),
+        text = element.innerText.split("");
+  
+  element.innerText = "";
+  
+  text.forEach((value, index) => {
+    const outer = document.createElement("span");
+    
+    outer.className = "outer";
+    
+    const inner = document.createElement("span");
+    
+    inner.className = "inner";
+    
+    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+    
+    const letter = document.createElement("span");
+    
+    letter.className = "letter";
+    
+    letter.innerText = value;
+    
+    letter.style.animationDelay = `${index * 1000 }ms`;
+    
+    inner.appendChild(letter);    
+    
+    outer.appendChild(inner);    
+    
+    element.appendChild(outer);
+  });
+}
+
+enhance("channel-link");
+enhance("channel-link1");
+enhance("channel-link2");
+enhance("channel-link3");
+
+
+
